ab39c0143ceae228151bc21be83826871e24f03b
PaulBGOVST <xbpb124@gmail.com>
2018-04-17 20:49:43 -0500

    butts

--- models/base.py
+++ models/base.py
@@ -5,8 +5,8 @@ env.read_envfile()
 env.read_envfile('.env')
 
 db_host = env.str('mysql_database_host')
-db_user = env.str('mysql_database_gsu1_username')
-db_password = env.str('mysql_database_gsu1_password')
+db_user = env.str('mysql_username')
+db_password = env.str('mysql_password')
 db_name = env.str('mysql_database_name')
 
 db = MySQLDatabase(host=db_host, user=db_user, password=db_password, database=db_name)
