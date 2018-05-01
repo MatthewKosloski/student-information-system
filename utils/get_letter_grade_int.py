cce802eef46e6ff1ab673ee439b402e1c38ffd12
= <matthewkosloski@me.com>
2018-05-01 15:25:28 -0500

    fixed grading scale

--- utils/get_letter_grade_int.py
+++ utils/get_letter_grade_int.py
@@ -7,13 +7,13 @@
 	@return {int}
 '''
 def get_letter_grade_int(percent_grade):
-	if percent_grade >= 90:
+	if percent_grade >= 90: # 90-100
 		return 1
-	elif percent_grade >= 80 and percent_grade <= 89:
+	elif percent_grade >= 80 and percent_grade < 90: # 80-89
 		return 2
-	elif percent_grade >= 70 and percent_grade <= 79:
+	elif percent_grade >= 70 and percent_grade < 80: # 70-79
 		return 3
-	elif percent_grade >= 60 and percent_grade <= 69:
+	elif percent_grade >= 60 and percent_grade < 70: # 60-69
 		return 4
-	else:
+	else: # 0-59
 		return 5
